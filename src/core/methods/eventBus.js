9babfee9d056820e634b29d470802b43fb603896
青衿 <122274389@qq.com>
2019-04-02 12:33:06 +0800

    eventBus的off方法支持指定事件处理函数

--- src/core/methods/eventBus.js
+++ src/core/methods/eventBus.js
@@ -5,18 +5,20 @@ export default {
       return console.error(
         `事件：${eventName}，被触发了，但是无监听者。事件内容为：${payload}`
       )
-    for (let i = 0; i < eventMap[eventName].length; i++) {
-      eventMap[eventName][i](payload)
+
+    for (let item of eventMap[eventName].values()) {
+      item(payload)
     }
   },
-  on(eventName, cb) {
-    if (typeof cb === 'function') {
-      if (!eventMap[eventName]) eventMap[eventName] = []
-      eventMap[eventName].push(cb)
+  on(eventName, handler) {
+    if (typeof handler === 'function') {
+      if (!eventMap[eventName]) eventMap[eventName] = new Set()
+      eventMap[eventName].add(handler)
     }
   },
-  off(eventName) {
-    eventMap[eventName] = []
+  off(eventName, handler) {
+    if (!handler) return console.error(`取消监听必须传入已被监听的事件处理函数`)
+    eventMap[eventName].delete(handler)
   },
   clean() {
     eventMap = {}

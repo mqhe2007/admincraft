5479e4cf6c375b02383f63fa543ec0ac1c02e515
青衿 <122274389@qq.com>
2019-04-03 15:48:29 +0800

    mutations方法抛出错误改为控制台打印

--- src/core/store/dynamicComponent.js
+++ src/core/store/dynamicComponent.js
@@ -14,7 +14,7 @@ export default {
           Vue.set(state[position], componentName, component)
         }
       } else {
-        throw new Error(
+        console.error(
           '插槽添加方法接受一个对象，必须配置position, component属性。'
         )
       }
@@ -22,12 +22,11 @@ export default {
     destroy(state, { position, name }) {
       if (position && name) {
         if (!state[position]) throw new Error(position + '插槽位置不存在')
-        if (!state[position][name]) throw new Error(position + '插槽中不存在组件' + name)
+        if (!state[position][name])
+          throw new Error(position + '插槽中不存在组件' + name)
         Vue.delete(state[position], name)
       } else {
-        throw new Error(
-          '插槽添加方法接受一个对象，必须配置position, name属性。'
-        )
+        console.error('插槽添加方法接受一个对象，必须配置position, name属性。')
       }
     }
   }

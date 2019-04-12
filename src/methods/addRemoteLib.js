90c2cc23819c41717febe09aec337c41af34ce1c
青衿 <122274389@qq.com>
2019-04-12 12:51:30 +0800

    使用同步方式插入远程js库

--- src/methods/addRemoteLib.js
+++ src/methods/addRemoteLib.js
@@ -1,39 +1,21 @@
-export default libArray =>
-  new Promise((resolve, reject) => {
-    let promiseAll = []
-    libArray.forEach(item => {
-      promiseAll.push(
-        new Promise((resolve, reject) => {
-          if (/\.js$/.test(item)) {
-            let script = document.createElement('script')
-            script.src = item
-            document.body.appendChild(script)
-            script.onload = () => {
-              resolve()
-            }
-            script.onerror = () => {
-              reject()
-            }
-          } else {
-            let link = document.createElement('link')
-            link.href = item
-            link.rel = 'stylesheet'
-            document.head.appendChild(link)
-            link.onload = () => {
-              resolve()
-            }
-            link.onerror = () => {
-              reject()
-            }
-          }
-        })
-      )
-    })
-    Promise.all(promiseAll)
-      .then(() => {
-        resolve()
-      })
-      .catch(() => {
-        reject()
-      })
+export default libArray => {
+  libArray.forEach(item => {
+    if (/\.js$/.test(item)) {
+      document.write(`<script src="${item}" defer></script>`)
+      // let script = document.createElement('script')
+      // script.setAttribute('src', item)
+      // document.head.appendChild(script)
+      // script.onload = () => {
+      //   resolve()
+      // }
+      // script.onerror = () => {
+      //   reject()
+      // }
+    } else {
+      let link = document.createElement('link')
+      link.href = item
+      link.rel = 'stylesheet'
+      document.head.appendChild(link)
+    }
   })
+}

71ff2cc6e7005cda45682f8827af971a6b001d97
孟庆贺 <122274389@qq.com>
2020-01-13 12:24:30 +0800

    优化

--- src/App.vue
+++ src/App.vue
@@ -1,24 +1,22 @@
 <template>
-  <div class="app"
-       v-cloak>
-    <component v-if="layout"
-               :is="layout"></component>
+  <div id="app" v-cloak>
+    <component v-if="layout" :is="layout"></component>
   </div>
 </template>
 
 <script>
 export default {
-  name: 'app',
+  name: "app",
   metaInfo() {
     return {
-      title: `${this.$route.meta.title || '未命名页面'}`,
-      titleTemplate: '%s | ' + this.$store.state.app.config.title
-    }
+      title: `${this.$route.meta.title || "未命名页面"}`,
+      titleTemplate: "%s | " + this.$store.state.app.config.title
+    };
   },
   data() {
     return {
-      layout: 'layoutDefault'
-    }
+      layout: "layoutDefault"
+    };
   },
   watch: {
     // 监听懒加载的路由对象，选择布局，不可用computed方法，因为懒加载是异步的。
@@ -26,17 +24,17 @@ export default {
       handler(n) {
         // console.log(n)
         // 当前路由未匹配到组件，布局为空
-        if (!n.matched.length) return (this.layout = '')
+        if (!n.matched.length) return (this.layout = "");
         this.layout =
           n.meta.layout ||
           (n.matched[0] && n.matched[0].meta.layout) ||
-          'layoutDefault'
+          "layoutDefault";
       },
       deep: true,
       immediate: true
     }
   }
-}
+};
 </script>
 
 <style>
@@ -52,10 +50,11 @@ body {
 [v-cloak] {
   display: none;
 }
-.app {
+#app {
   width: 100vw;
   height: 100vh;
   overflow: hidden;
+  display: none;
 }
 </style>
 
